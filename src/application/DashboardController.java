ea5a6572d871ebb7a45b2e9306fa253d0da36d8f
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2021-03-19 20:33:14 -0700

    Retrive appointment data from db

--- src/application/DashboardController.java
+++ src/application/DashboardController.java
@@ -2,36 +2,55 @@ package application;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 
 import connectivity.UserSession;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 
-public class DashboardController extends AnchorPane {
-	
+public class DashboardController extends BorderPane {
+
 	UserSession currSession;
+
+	@FXML
+	Text greetingText;
+	TableView apptTable;
+
+	public DashboardController() {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Dashboard.fxml"));
+
+		fxmlLoader.setRoot(this);
+		fxmlLoader.setController(this);
+		
+		currSession = UserSession.getInstance();
+
+		try {
+			fxmlLoader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	void initialize() throws SQLException {
+		greetingText.setText("Hello, " + currSession.getFirstName());
+		populateApptTable();
+		
+	}
 	
-	@FXML Text greetingTextLabel;
+	void populateApptTable() throws SQLException {
+		
+		List<String[]> appointments = currSession.getAppt(currSession.getEmail());
+		
+		for (String[] row : appointments) {
+			System.out.println(Arrays.toString(row));
+			
+		}
 
-    public DashboardController(){
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Dashboard.fxml"));
-
-        fxmlLoader.setRoot(this);
-        fxmlLoader.setController(this);
-        
-        try {
-            fxmlLoader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    @FXML
-    void initialize() throws SQLException {
-    	currSession = UserSession.getInstance();
-    	greetingTextLabel.setText("Hello, " + currSession.getFirstName());
 	}
 
 }
