a2ff764744fa3b3a23474ca62269c57a4a2e5eee
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2021-04-11 20:45:07 -0700

    Changed DB connection

--- src/connectivity/ConnectionClass.java
+++ src/connectivity/ConnectionClass.java
@@ -2,26 +2,35 @@ package connectivity;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConnectionClass {
+
+	public Connection conn;
 	
-	public Connection connection;
-	
+	private String userName = "root";
+	private String password = "Password123";
+
+	private static final String DB_URL = "jdbc:mysql://localhost/EduBook";
+
 	public Connection getConnection() {
-		
-		String dbName = "EduBook";
-		String userName="root";
-		String password="Password123";
-		
+
 		try {
+			// STEP 2: Register JDBC driver
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/" + dbName,userName,password);
-			
+			// STEP 3: Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, userName, password);
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
 		} catch (Exception e) {
+			// Handle errors for Class.forName
 			e.printStackTrace();
 		}
-		
-		return connection;
+
+		return conn;
 	}
 }
